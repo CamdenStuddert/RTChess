e955f8020cc2a4671a6497023076a05ece30133c
Camden Studdert <82480725+CamdenStuddert@users.noreply.github.com>
2024-05-10 08:38:04 -0600

    Implemented Movement for King

--- RTChess/RTChess/Model/King.swift
+++ RTChess/RTChess/Model/King.swift
@@ -15,6 +15,32 @@ struct King: Piece {
     }
     
     func getAvailableMoves(board: Board) -> [Move] {
-        return []
+        
+        let location = Board.getLocation(at: position)
+        var moves: [Move] = []
+        let possibleMoves: [Move] = [
+            .available(x: location.x + 1, y: location.y + 1),
+            .available(x: location.x + 1, y: location.y - 1),
+            .available(x: location.x + 1, y: location.y),
+            .available(x: location.x, y: location.y - 1),
+            .available(x: location.x, y: location.y + 1),
+            .available(x: location.x - 1, y: location.y - 1),
+            .available(x: location.x - 1, y: location.y + 1),
+            .available(x: location.x - 1, y: location.y),
+        ]
+        outer: for move in possibleMoves {
+            for piece in board.pieces{
+                if (piece.target == nil && piece.location.x == move.x && piece.location.y == move.y) {
+                    if piece.team != team {
+                        moves.append(.attack(x: move.x, y: move.y, id: piece.id))
+                    }
+                    continue outer
+                }
+            }
+            if move.y <= (Board.cells - 1) && move.y >= 0 && move.x >= 0 && move.x <= (Board.cells - 1) {
+                moves.append(.available(x: move.x, y: move.y))
+            }
+        }
+        return moves
     }
 }
