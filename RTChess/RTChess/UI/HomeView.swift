6e34be120f72193ca6deb1fbf4fa1ca0a1ff6c25
Camden Studdert <82480725+CamdenStuddert@users.noreply.github.com>
2024-05-13 16:59:23 -0600

    Background Tings

--- RTChess/RTChess/UI/HomeView.swift
+++ RTChess/RTChess/UI/HomeView.swift
@@ -9,29 +9,48 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        NavigationView {
-            VStack{
-                Image("PracticeLogo")
-                    .resizable()
-                    .padding()
-                    .scaledToFit()
-            
 
-                NavigationLink(destination: HostJoinView()) {
-                    Text("Host/Join")
-                        .foregroundColor(.black)
-                        .font(.system(size: 40))
+            
+            NavigationView {
+                GeometryReader { geo in
+                    
+                    Image("Background")
+                        .resizable()
+                        .scaledToFill()
+                        .edgesIgnoringSafeArea(.all)
+                        .frame(width: geo.size.width, height: geo.size.height, alignment: .center)
+                    
+                VStack{
+                    Image("PracticeLogo")
+                        .resizable()
                         .padding()
-                        .border(.black, width: 4)
-                        .padding()
-                }
-                NavigationLink(destination: SettingsView()) {
-                    Text("Settings")
-                        .foregroundColor(.black)
-                        .font(.system(size: 40))
-                        .padding()
-                        .border(.black, width: 4)
-                        .padding()
+                        .scaledToFit()
+                    
+                    
+                    NavigationLink(destination: HostJoinView()) {
+                        Text("Host/Join")
+                            .foregroundColor(.black)
+                            .font(.system(size: 40))
+                            .padding()
+                            .border(.black, width: 4)
+                            .padding()
+                    }
+                    NavigationLink(destination: OneDeviceView()) {
+                        Text("Table Match")
+                            .foregroundColor(.black)
+                            .font(.system(size: 40))
+                            .padding()
+                            .border(.black, width: 4)
+                            .padding()
+                    }
+                    NavigationLink(destination: SettingsView()) {
+                        Text("Settings")
+                            .foregroundColor(.black)
+                            .font(.system(size: 40))
+                            .padding()
+                            .border(.black, width: 4)
+                            .padding()
+                    }
                 }
             }
         }
---
+++ RTChess/RTChess/UI/OneDeviceView.swift
@@ -0,0 +1,18 @@
+//
+//  OneDeviceView.swift
+//  RTChess
+//
+//  Created by Camden Studdert on 5/13/24.
+//
+
+import SwiftUI
+
+struct OneDeviceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OneDeviceView()
+}
